d48a28f581322530bdc73c3480aca59d401f3cd2
Joey Borrelli <joeyjrelli3824@gmail.com>
2024-03-19 21:04:22 -0500

    Lab7 finished

--- Labs/Lab7/Queue.h
+++ Labs/Lab7/Queue.h
@@ -15,55 +15,102 @@ class Queue{
     private:
         struct QueueNode{
             std::string riderName;
-            QueueNode* next = nullptr;
+            QueueNode* next; // points to next node
+
+            QueueNode(std::string riderName){
+                next = nullptr;
+                this->riderName = riderName;
+            }
         };
 
-        int numNodes;
-        QueueNode* head;
-        QueueNode* tail;
+        int numNodes; // total nodes
+        QueueNode* head; // front of the line
+        QueueNode* tail; // back of the line
     public:
+        /*Function prototypes*/
         Queue();
         ~Queue();
         void enqueue(std::string);
-        void dequeue();
+        std::string dequeue();
         int getNumNodes();
         bool isEmpty();
+        QueueNode* getDesiredNode(int);
 };
 
 Queue::Queue(){
+    // init all zero/null
     head = nullptr;
     tail = nullptr;
     numNodes = 0;
 }
 
 Queue::~Queue(){
-    /*NEEDS TO LOOP THROUGH QUEUE*/
-    std::cout << "Oh shoot, I am sorry, our park is closed so " + head->riderName + "will not get to ride today.\n";
-}
-
-void Queue::enqueue(std::string riderName){
-    QueueNode *newNode;
-    newNode->riderName = riderName;
-    
-    if(isEmpty){
-        this->head = newNode;
-        this->tail = newNode;
-    }else{
+    while(head){ // while there are nodes in queue
+        QueueNode* nodeToDelete = this->head; // set node to first in line
         
+        std::cout << "Oh shoot, I am sorry, our park is closed so " + nodeToDelete->riderName + " will not get to ride today.\n";
+        
+        // set our head to the next in line
+        this->head = this->head->next;
+
+        // delete previous head
+        delete nodeToDelete;
     }
 }
 
-void Queue::dequeue(){
+void Queue::enqueue(std::string riderName){
+    // create node
+    QueueNode* newNode = new QueueNode(riderName);
 
+    // insert into queue
+    if(isEmpty()){ // if there is nothing in the queue
+        this->head = newNode;
+        this->tail = newNode;
+    }else{ // if we need to add the dude to the end of the queue
+        QueueNode* previousNode = getDesiredNode(getNumNodes() - 1);
+        previousNode->next = newNode;
+        this->tail = newNode;
+    }
+
+    // incr total
+    this->numNodes++;
 }
 
+std::string Queue::dequeue(){
+    // find our node to return
+    QueueNode* nodeToReturn = this->head;
+    
+    // set our head to the next in line
+    this->head = this->head->next;
+
+    // decrement our total 
+    this->numNodes--;
+
+    // return our rider's name
+    return(nodeToReturn->riderName);
+}
+
+// returns the total number of nodes
 int Queue::getNumNodes(){
     return(numNodes);
 }
 
+// returns if our queue are empty or not
 bool Queue::isEmpty(){
     return(!head);
 }
 
+// returns a pointer to a node at our desired position. (first index is 0)
+Queue::QueueNode* Queue::getDesiredNode(int desiredNodePos){
+    QueueNode* tempNode = head;
+    // loop through function, search for match and return node
+    for(int i = 0; i <= getNumNodes(); i++){ 
+        if(i == desiredNodePos){
+            return(tempNode);
+        }else{
+            tempNode = tempNode->next; // set tempNode to next in queue
+        }
+    }
+}
 
 #endif
--- Labs/Lab7/lab7.cpp
+++ Labs/Lab7/lab7.cpp
@@ -5,39 +5,72 @@
     GLORIA PATRI, ET FILIO, ET SPIRITUI SANCTO. SICUT ERAT IN PRINCIPIO, ET NUNC, ET SEMPER, ET IN SAECULA SAECULORUM
 */
 
-#include <iostream>
-#include <string>
 #include "Queue.h"
 
 /*function prototypes*/
 void addRiderToLine(std::string, int);
 void removeRiderFromQueue();
 
+/*global queues*/
+Queue* normalPassQueue = new Queue();
+Queue* instantPassQueue = new Queue();
+
 int main(){
     std::string name;
     int userInput, passHoldingStatus;
     bool isDone = false;
-    std::cout << "What you want to do?\nEnter 1 for adding a rider to the line\nEnter 2 for removing a rider from the line\nEnter 3 to quit\nEnter: ";
-    std::cin >> userInput;
-    std::cout << "\n\n";
 
     while(!isDone){
+        std::cout << "\n1)      ADD RIDER TO WAIT LINE.";
+        std::cout << "\n2)      REMOVE RIDER FROM LINE.";
+        std::cout << "\n3)      DONE FOR THE DAY.";
+        std::cout << "\nCHOOSE 1-3: ";
+        std::cin >> userInput;
+        
+        // input verification
+        if(userInput < 0 || userInput > 3){
+            while(userInput < 0 || userInput > 3){
+                std::cout << "\nOOPS! That ain't right. CHOOSE 1-3: ";
+                std::cin >> userInput;
+            }
+        }else{
+            std::cout << "\n";
+        }
+    
         switch (userInput){
             case 1:
                 std::cout << "What is your name? ";
-                std::cin >> name;
-                std::cout << "\nAre you a normal pass holder(1), or an instant pass holder(2)? ";
+                std::cin.ignore();
+                getline(std::cin, name);
+                std::cout << "\n" << name << ", do you have a(n):";
+                std::cout << "\n1) Normal Pass?";
+                std::cout << "\n2) Instant Pass?";
+                std::cout << "\nCHOOSE 1 OR 2: ";
                 std::cin >> passHoldingStatus;
+                
+                // input verification
+                if(passHoldingStatus != 1 && passHoldingStatus != 2){ 
+                    while(passHoldingStatus != 1 && passHoldingStatus != 2){
+                        std::cout << "\nOOPS! That ain't right. CHOOSE 1 or 2: ";
+                        std::cin >> passHoldingStatus;
+                    }
+                }else{
+                    std::cout << "\n";
+                }
+                
                 addRiderToLine(name, passHoldingStatus);
                 break;
             case 2:
                 removeRiderFromQueue();
                 break;
-            case 3:
-                std::cout << "Ok you done.\n";
+            case 3: // quit case
+                std::cout << "**IT IS QUITTING TIME!**\n\n";
                 isDone = true;
+                delete normalPassQueue;
+                delete instantPassQueue;
                 break;
             default:
+                std::cout << "\nERROR: Default case hit in main switch statement lab7.cpp\n";
                 break;
         }
     }
@@ -46,10 +79,20 @@ int main(){
 }
 
 void addRiderToLine(std::string riderName, int riderPassStatus){
-
+    if(riderPassStatus == 1){ // if rider has normal pass
+        normalPassQueue->enqueue(riderName);
+    }else{ // if rider has instant pass
+        instantPassQueue->enqueue(riderName);
+    }
 }
 
 void removeRiderFromQueue(){
     // remove a rider from the instant pass queue if there are people there or remove from normal queue.
+    if(instantPassQueue->isEmpty() && !normalPassQueue->isEmpty()){ // if only normal queue is populated
+        std::cout << "Dequeuing: " + normalPassQueue->dequeue() << "\n";
+    }else if(instantPassQueue->isEmpty() && normalPassQueue->isEmpty()){ // if both queues are empty
+        std::cout << "THERE IS NO ONE IN LINE!\n";
+    }else{ // if only instant queue is populated
+        std::cout << "Dequeuing: " + instantPassQueue->dequeue() << "\n";
+    }
 }
-
